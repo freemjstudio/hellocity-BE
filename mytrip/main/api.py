36cf47977d1ab6a93f4bb3e8f83564e1bf64a4c4
freemjstudio <mjwoo0@naver.com>
2023-06-08 00:02:31 +0900

    [Django] CREATE TABLE to sqlite

--- mytrip/main/api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pandas as pd
-from urllib.parse import urlencode, unquote, quote_plus
-from bs4 import BeautifulSoup
-
-# api 에서 데이터 가져오기
-serviceKey = ""
-
--- mytrip/main/models.py
+++ mytrip/main/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+# schema 정의
+class hotel(models.Model):
