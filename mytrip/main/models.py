42b493acbce4a91f2370cc6837aa79d9c8f200ae
freemjstudio <mjwoo0@naver.com>
2023-06-08 00:12:11 +0900

    [Django] CREATE MODEL

--- mytrip/main/models.py
+++ mytrip/main/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 # schema 정의
 class hotel(models.Model):
+    id = models.IntegerField()
+    FCLTY_NM = models.CharField()
+    RDNMADR_NM = models.CharField()
+    CTPRVN_CD = models.IntegerField()
+    SIGNGU_CD = models.IntegerField()
+    FCLTY_LO = models.FloatField()
+    FCLTY_LA = models.FloatField()
+    AREA_NM = models.CharField()
+    HMPG_URL = models.CharField()
+    HOTEL_GRAD_NO = models.CharField()
+    TEL_NO = models.CharField()
+    GSRM_CO = models.FloatField()
+    GSRM_AR_VALUE = models.CharField()
+    GSRM_SVC_POSBL_AT = models.CharField()
+    DSPSN_FCLTY_AT = models.CharField()
+    BASE_DE = models.CharField()
