b8724a322093da5b5aa2a37da5aec846ed735739
freemjstudio <mjwoo0@naver.com>
2023-06-10 21:57:07 +0900

    remove log

--- mytrip/main/views.py
+++ mytrip/main/views.py
@@ -11,47 +11,39 @@ from django.core import serializers
 def index(request):
     return HttpResponse(" hello city team backend")
 
-class hotelViewSet(viewsets.ModelViewSet):
-    queryset = barrier_free_hotel.objects.all()
-    serializer_class = HotelSerializer
-
-class itemViewSet(viewsets.ModelViewSet):
-    queryset = categorized.objects.all()
-    serializer_class = CategorySerializer
-
 
 def hotelListAPI(request):
     hotel_list = barrier_free_hotel.objects.all().values()
     context = {'hotel_list':hotel_list}
-    print(context)
+    # print(context)
     return render(request, 'main/hotel_list.html', context)
 
 def dataListAPI(request):
     data_list = categorized.objects.all().values()
     context = {'data_list':data_list}
-    print(context)
+    # print(context)
     return render(request, 'main/data_list.html', context)
 
 def activityAPI(request):
     activity_list = categorized.objects.filter(category='액티비티').values()
     context = {'activity_list': activity_list}
-    print(context)
+    # print(context)
     return render(request, 'main/activity_list.html', context)
 
 def healingAPI(request):
     healing_list = categorized.objects.filter(category='힐링').values()
     context = {'healing_list': healing_list}
-    print(context)
+    # print(context)
     return render(request, 'main/healing_list.html', context)
 
 def restaurantAPI(request):
     restaurant_list = categorized.objects.filter(category='맛집').values()
     context = {'restaurant_list':restaurant_list}
-    print(context)
+    # print(context)
     return render(request, 'main/restaurant_list.html', context)
 
 def exploreAPI(request):
     explore_list = categorized.objects.filter(category='탐구').values()
     context = {'explore_list':explore_list}
-    print(context)
+    # print(context)
     return render(request, 'main/explore_list.html', context)
